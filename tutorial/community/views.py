92ce6eb157ebdb18c00530587e4de84fcb74267d
nomadlife <joonwoo3816@gmail.com>
2018-03-15 20:31:45 +0900

    tutorial example added

---
+++ tutorial/community/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from community.forms import *
+
+# Create your views here.
+
+def write(request):
+	if request.method == 'PSOT':
+		form = Form(request.POST)
+		if form.is_valid():
+			form.save()
+	else:
+		form = Form()
+
+	return render(request, 'write.html', {'form':form})
