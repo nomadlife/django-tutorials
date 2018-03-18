36a1380511e51de73e9de86295b080bfce1ba91b
nomadlife <joonwoo3816@gmail.com>
2018-03-18 17:19:16 +0900

    django tutorial ex3 updated(~ch23)

--- ex3/music/views_old3(try).py
+++ ex3/music/views_old3(try).py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
+
+# Create your views here.
 
 def index(request):
     all_albums = Album.objects.all()
     return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
+    try :
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
     return render(request, 'music/detail.html', {'album':album})
