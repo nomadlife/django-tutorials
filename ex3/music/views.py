aa41fdbd2b3c89fad63bcf0bd7e9797af14cd126
nomadlife <joonwoo3816@gmail.com>
2018-03-20 23:28:56 +0900

    ex3 tutorial ch34, user registration, ongoing

--- ex3/music/views.py
+++ ex3/music/views.py
@@ -1,7 +1,12 @@
-from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenicate, login
+from django.views import generic
+from django.views.generic import View
 from .models import Album
+from .forms import UserForm
+
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -24,3 +29,9 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+class UserFormView(view):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    def get(self, request):
