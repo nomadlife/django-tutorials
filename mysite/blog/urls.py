0fb8bbaceb141068bf2f2a84010daf8fcaa9e328
nomadlife <joonwoo3816@gmail.com>
2018-03-22 15:40:34 +0900

    ex1 tutorial ~ch10 finished. skip ch11,12.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,4 +2,8 @@ from django.urls import path, include
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-urlpatterns = [path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],template_name="blog/blog.html"))]
+urlpatterns = [
+    path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],template_name="blog/blog.html")),
+    path('<pk>/', DetailView.as_view(model=Post, template_name='blog/post.html'))
+
+]
