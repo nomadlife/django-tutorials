f2e6c0c152fb61ec0ef9700740529768bf3e7a0a
nomadlife15u <aaeacd@gmail.com>
2018-10-22 18:15:46 +0900

    database models (error issue not solved)

--- tutorial5/blog/views.py
+++ tutorial5/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'person1',
-        'title':'post 1',
-        'content':'1st post content',
-        'date_posted':'August 27, 2018'
-    },
-    {
-        'author':'person2',
-        'title':'post 2',
-        'content':'2nd post content',
-        'date_posted':'August 28, 2018'
-    }
-]
+from .models import Post
 
 # Create your views here.
+
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
