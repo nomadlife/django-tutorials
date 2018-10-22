58c627eb57bfcc44f5fdc4b948c42e80d2fb7b33
nomadlife15u <aaeacd@gmail.com>
2018-10-22 14:19:36 +0900

    template + variables

--- tutorial5/blog/views.py
+++ tutorial5/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author':'person1',
+        'title':'post 1',
+        'content':'1st post content',
+        'date_posted':'August 27, 2018'
+    },
+    {
+        'author':'person2',
+        'title':'post 2',
+        'content':'2nd post content',
+        'date_posted':'August 28, 2018'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
