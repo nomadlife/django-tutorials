ac6d20b7ac385d721f48e03b78476ae705d9def6
nomadlife15u <aaeacd@gmail.com>
2018-10-23 16:01:16 +0900

    profile / media / signal

--- tutorial5/tutorial5/urls.py
+++ tutorial5/tutorial5/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views # prevent name colide
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views # prevent name colide
 
 urlpatterns = [
@@ -27,3 +29,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
